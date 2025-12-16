60ff615b33116304c9abb3b8272068b8d074e359
Eugene Goncharov <eugenegoncharov@Eugenes-MacBook-Air.local>
2025-12-16 13:11:20 -0800

    Implement system vs derived focus tokens and runtime mapping for Button

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -2,6 +2,12 @@
  * Do not edit directly, this file was auto-generated.
  */
 
+export const Env2UiButtonFocusRingColorDerived = "#066a8d";
+export const Env2UiButtonPrimaryBackgroundBase = "#066a8d";
+export const Env2UiButtonPrimaryLabelBase = "#ffffff";
+export const Env2UiButtonSecondaryBackgroundBase = "#ffffff";
+export const Env2UiButtonSecondaryLabelBase = "#1b1b1b";
+export const Env2UiButtonSecondaryBorderBase = "#cccccc";
 export const Env2UiColorBrandPrimary = "#066a8d";
 export const Env2UiColorNeutral100 = "#f1f2f7";
 export const Env2UiColorNeutral300 = "#cccccc";
@@ -18,11 +24,8 @@ export const Env2UiColorPrimitivesNeutral500 = "#757575";
 export const Env2UiColorPrimitivesNeutral700 = "#404040";
 export const Env2UiColorPrimitivesNeutral900 = "#1b1b1b";
 export const Env2UiColorPrimitivesNeutralWhite = "#ffffff";
-export const Env2UiButtonPrimaryBackgroundBase = "#066a8d";
-export const Env2UiButtonPrimaryLabelBase = "#ffffff";
-export const Env2UiButtonSecondaryBackgroundBase = "#ffffff";
-export const Env2UiButtonSecondaryLabelBase = "#1b1b1b";
-export const Env2UiButtonSecondaryBorderBase = "#cccccc";
+export const Env2UiColorSystemFocus = "#ff4d00";
+export const Env2UiFocusRingColorSystem = "#ff4d00";
 export const Env2UiFocusRingWidthBase = "2px";
 export const Env2UiFocusRingWidthAccessible = "3px";
 export const Env2UiRadiusPill = "999px";
