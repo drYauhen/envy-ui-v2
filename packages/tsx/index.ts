c3c45edc1ccd00fec2c9cb711f46f353c8d89600
Eugene Goncharov <eugenegoncharov@Eugenes-MacBook-Air.local>
2025-12-24 06:39:38 -0800

    chore: update generated tokens and exports
    - Regenerate tokens after calendar and table updates
    - Add calendar exports to TSX package index

--- packages/tsx/index.ts
+++ packages/tsx/index.ts
@@ -1,5 +1,6 @@
 export { ButtonClean } from './button';
 export { ButtonGroup } from './button';
 export type { ButtonCleanProps } from './button';
+export * from './calendar';
 export * from './menu';
 export * from './icon';
